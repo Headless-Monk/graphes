3754fbca262e5c0e5f0a0a8dc8a3c105526ee602
leonard <lievin.leonard@gmail.com>
2018-03-30 12:29:02 +0200

    mise en place structure fs aps

--- FsAps.cpp
+++ FsAps.cpp
@@ -1,1 +1,28 @@
 #include "FsAps.h"
+
+FsAps::FsAps(vector<int> fs, vector<int> aps)
+{
+    d_fs = fs ;
+    d_aps = aps ;
+}
+
+vector <int> FsAps::getFs() const
+{
+    return d_fs;
+}
+vector <int> FsAps::getAps() const
+{
+    return d_aps;
+}
+int FsAps::longueurFs()const
+{
+    return d_fs.size();
+}
+int FsAps::longueurAps() const
+{
+     return d_aps.size();
+}
+int FsAps::nombreArc()const
+{
+    return (longueurFs()-longueurAps());
+}
--- FsAps.h
+++ FsAps.h
@@ -1,13 +1,25 @@
 #ifndef FSAPS_H
 #define FSAPS_H
 
+#include <iostream>
+#include <vector>
+
+using namespace std ;
+
 class FsAps
 {
     public:
-        
+
+        FsAps(vector<int>, vector<int>);
+        vector <int> getFs() const;
+        vector <int> getAps() const;
+        int longueurFs() const;
+        int longueurAps() const ;
+        int nombreArc() const;
 
     private:
-        
+    	vector <int> d_fs ;
+    	vector <int> d_aps ;
 };
 
 
